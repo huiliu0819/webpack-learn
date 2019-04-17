8de6e761512ddde84af4604ddd0fb0f722db2781
刘辉 <liuhui@liuhuideMacBook-Pro.local>
2019-04-17 14:30:03 +0800

    master

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
+const arr = [
+  new Promise(() => {}),
+  new Promise(() => {})
+]
 
-import tag from './price_tag@2x.png'
-import style from './index.scss'
-import createTag from './createTag'
-createTag()
-let img = new Image()
-img.src = tag
-img.classList.add(style.tag)
-img.classList.add(style.aaa)
-
-let root = document.getElementById('root')
-root.append(img)
-
+arr.map(item => {
+  console.log(item)
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,81 @@
 const path = require('path')
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const webpack = require('webpack')
+// presets: [
+  //   [
+  //     '@babel/preset-env', 
+  //     {
+  //       targets: {
+  //         chrome: "67",
+  //       },
+  //       useBuiltIns: 'usage',
+  //       corejs: 3
+  //     },
+  //   ]
+  // ]
 module.exports = {
   mode: 'development',
+  devtool: 'cheap-module-eval-source-map', 
   entry: {
-    main: './src/index.js',
+    main: './src/index.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    open: true,
+    port: 3000,
+    hot: true,
+    hotOnly: true
   },
   module: {
     rules: [
       {
-      test: /\.(png|jpg|gif)$/,
-      use: {
-        loader: 'url-loader',
-        options: {
-          name: '[name]_[hash].[ext]',
-          outputPath: 'images/',
-          limit: 204800
+        test: /\.(png|jpg|gif)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: '[name]_[hash].[ext]',
+            outputPath: 'images/',
+            limit: 204800
+          }
         }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', 
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 2,
+            modules: true
+          }
+        }, 
+        'sass-loader', 
+        'postcss-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', 
+          'css-loader',
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
       }
-    },
-    {
-     test: /\.scss$/,
-     use: [
-       'style-loader', 
-       {
-         loader: 'css-loader',
-         options: {
-           importLoaders: 2,
-           modules: true
-         }
-       }, 
-       'sass-loader', 
-       'postcss-loader']
-    }
-  ]
+    ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({template:'./index.html'}),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
   },
-  // plugins: [new HtmlWebpackPlugin()]
 }
